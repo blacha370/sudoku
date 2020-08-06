e0cff3732fe2f91abcd2e2ef1fea663c1f233326
Łukasz Blacha <lukasz.blacha370@gmail.com>
2020-08-06 21:06:21 +0200

    develop solving algorithm

--- Sudoku.py
+++ Sudoku.py
@@ -6,16 +6,16 @@ class Sudoku:
         return [element[position[1]]
                 for element in board]
 
-    def get_horizontal_box_line(self, position, x, board):
+    def get_horizontal_box_line(self, position, y, board):
         box = list()
         if position[1] <= 2:
-            for y in range(3):
+            for x in range(3):
                 box.append(board[y][x])
         elif position[1] <= 5:
-            for y in range(3, 6):
+            for x in range(3, 6):
                 box.append(board[y][x])
         elif position[1] <= 8:
-            for y in range(6, 9):
+            for x in range(6, 9):
                 box.append(board[y][x])
         else:
             return False
@@ -24,17 +24,31 @@ class Sudoku:
     def get_box(self, position, board):
         box = list()
         if position[0] <= 2:
-            for x in range(3):
-                for element in self.get_horizontal_box_line(position, x, board):
+            for y in range(3):
+                for element in self.get_horizontal_box_line(position, y, board):
                     box.append(element)
         elif position[0] <= 5:
-            for x in range(3, 6):
-                for element in self.get_horizontal_box_line(position, x, board):
+            for y in range(3, 6):
+                for element in self.get_horizontal_box_line(position, y, board):
                     box.append(element)
         elif position[0] <= 8:
-            for x in range(6, 9):
-                for element in self.get_horizontal_box_line(position, x, board):
+            for y in range(6, 9):
+                for element in self.get_horizontal_box_line(position, y, board):
                     box.append(element)
         else:
             return False
         return box
+
+    def solve_cell(self, board, position):
+        usedNumbers = set()
+        usedNumbers = usedNumbers | set(self.get_row(position, board))
+        usedNumbers = usedNumbers | set(self.get_column(position, board))
+        usedNumbers = usedNumbers | set(self.get_box(position, board))
+        i = board[position[0]][position[1]] + 1
+        while True:
+            if i not in usedNumbers and i <= 9:
+                return i
+            elif i > 9:
+                return False
+            else:
+                i += 1
--- my_program.py
+++ my_program.py
@@ -1,44 +1,39 @@
 from Board import SudokuBoard
 from Sudoku import Sudoku
-from copy import deepcopy
+from pprint import pprint
+
 sudoku_board_example = (
-    [3, 0, 6, 5, 0, 8, 4, 0, 0],
-    [5, 2, 0, 0, 0, 0, 0, 0, 0],
-    [0, 8, 7, 0, 0, 0, 0, 3, 1],
-    [0, 0, 3, 0, 1, 0, 0, 8, 0],
-    [9, 0, 0, 8, 6, 3, 0, 0, 5],
-    [0, 5, 0, 0, 9, 0, 6, 0, 0],
-    [1, 3, 0, 0, 0, 0, 2, 5, 0],
-    [0, 0, 0, 0, 0, 0, 0, 7, 4],
-    [0, 0, 5, 2, 0, 6, 3, 0, 0])
+    [1, 0, 7, 0, 0, 0, 5, 0, 0],
+    [0, 0, 0, 9, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 3, 0, 2, 1],
+    [0, 0, 0, 0, 0, 7, 9, 0, 5],
+    [4, 0, 0, 0, 0, 1, 0, 6, 0],
+    [0, 5, 0, 2, 0, 9, 0, 0, 3],
+    [0, 8, 1, 0, 0, 4, 0, 0, 0],
+    [0, 4, 3, 0, 5, 0, 0, 0, 6],
+    [0, 0, 0, 0, 0, 0, 0, 5, 0])
 
 sudoku = SudokuBoard(sudoku_board_example)
 Sudoku = Sudoku()
 
 
 def sudokuSolver(board):
-    edited_board = deepcopy(board)
+    to_do_cells = []
+    done_cells = []
     for y in range(9):
         for x in range(9):
             if board[y][x] == 0:
-                row = Sudoku.get_row((y, x), edited_board)
-                column = Sudoku.get_column((y, x), edited_board)
-                box = Sudoku.get_box((y, x), edited_board)
-                print((x, y), row, column, box)
-                i = 1
-                while True:
-                    print(i)
-                    if i not in row and i not in column and i not in box:
-                        edited_board[y][x] = i
-                        break
-                    elif i == 9:
-                        edited_board[y][x] = "error"
-                        break
-                    else:
-                        i += 1
-            else:
-                print('value')
-    print(edited_board)
+                to_do_cells.append((y, x))
+    while len(to_do_cells) > 0:
+        current_cell = to_do_cells[0]
+        result = Sudoku.solve_cell(board, current_cell)
+        if result:
+            board[current_cell[0]][current_cell[1]] = result
+            done_cells.append(to_do_cells.pop(0))
+        else:
+            board[current_cell[0]][current_cell[1]] = 0
+            to_do_cells.insert(0, done_cells.pop())
+    pprint(board)
 
 
 sudokuSolver(sudoku.board)
