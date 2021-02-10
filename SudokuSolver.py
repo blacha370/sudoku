6b8e016c6e74c943afb373c5fe80374b6087b49b
blacha370 <lukasz.blacha370@gmail.com>
2021-02-10 19:01:48 +0100

    Created SudokuSolver class and it's methods

---
+++ SudokuSolver.py
@@ -0,0 +1,36 @@
+class SudokuSolver:
+    @staticmethod
+    def validate_board(board: str):
+        pass
+
+    @staticmethod
+    def get_row(board: str, position: int):
+        pass
+
+    @staticmethod
+    def get_column(board: str, position: int):
+        pass
+
+    @staticmethod
+    def get_box(board: str, position: int):
+        pass
+
+    @staticmethod
+    def check_position(board: str, position: int):
+        pass
+
+    @staticmethod
+    def check_board(board: str):
+        pass
+
+    @staticmethod
+    def find_unsolved_positions(board: str):
+        pass
+
+    @staticmethod
+    def solve_cell(board: str, position: int):
+        pass
+
+    @staticmethod
+    def solve_board(board: str):
+        pass
